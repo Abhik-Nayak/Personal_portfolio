367aa95964a58a02b363804a324f1c12b33c6afd
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-04-13 18:25:45 +0530

    Abhik//13-04-2023

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Contact from './components/Contact';
+import Hero from './components/Hero';
+import Who from './components/Who';
+import Works from './components/Works';
+import styled from 'styled-components';
+import Home from "./assets/home.jpg";
+import Navbar from './components/Navbar';
+const Container = styled.div`
+height: 100vh;
+scroll-snap-type: y mandatory;
+scroll-behavior: smooth;
+overflow-y: auto;
+scrollbar-width: none;
+color: white;
+background: rgb(0,212,255);
+background-image: linear-gradient(to top, #30cfd0 0%, #330867 0%);
+&::-webkit-scrollbar{
+  dsiplay: none;
+}
+`;
+const App = () => {
+  return (
+    <Container>
+      {/* <Navbar/> */}
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </Container>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled from 'styled-components';
+import Search from '../assets/search.png';
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  overflow:hidden;
+  position:fixed;
+  background: rgb(0,212,255);
+background-image: linear-gradient(to top, #30cfd0 0%, #330867 0%);
+&::-webkit-scrollbar{
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Container = styled.div`
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Logo = styled.h3`
+  // height: 50px;
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo>Logo</Logo>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Studio</ListItem>
+            <ListItem>Works</ListItem>
+            <ListItem>Contact</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          {/* Changed the image due to copyright problems */}
+          <Icon src={Search} />
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
+    </Section>
+
+  )
+}
+
+export default Navbar
