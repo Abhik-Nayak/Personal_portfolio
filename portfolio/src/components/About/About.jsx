ece5c38d1ff52a4fd3ee0164d611cf2a8e50aefe
Abhik Nayak <abhiknayakabhik@gmail.com>
2023-09-11 09:07:06 +0530

    Abhik// ADD CSS

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -32,11 +32,11 @@ const About = () => {
             </div>
           </div>
           <div className="colAbout">
-            <div className="skills-box">
-              <div className="skills-header">
+            <div className="skills-box1">
+              <div className="skills-header-about">
                 <h3>UI Web Tech.</h3>
               </div>
-              <div className="skills-list">
+              <div className="skills-list-about" style={{ justifyContent: "space-around" }}>
                 <img className='tag' src="https://camo.githubusercontent.com/90b284b4f542180aeb3bfa244cf0a86d451f9ebc95f9cffe71dc8f73da41b7a7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f68746d6c352d2532334533344632362e7376673f7374796c653d706c6173746963266c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465" alt="HTML" />
                 <img className='tag' src="https://camo.githubusercontent.com/57909719a287181a7ed571f0df7fec7bf5b51a0833eb85bdac822f2184984edd/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f637373332d2532333135373242362e7376673f7374796c653d706c6173746963266c6f676f3d63737333266c6f676f436f6c6f723d7768697465" alt="CSS" />
                 <img className='tag' src="https://camo.githubusercontent.com/6d3b128cd950dff725d5caed37ed86c4039eeec0a861332bd2bc7a73fac74fd1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d706c6173746963266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145" alt="javaScript"/>
@@ -50,42 +50,42 @@ const About = () => {
                 <img className='tag' src="https://camo.githubusercontent.com/3f335350d2f798d1ee6dfd5503fbd56c88d41f9d28fb77d1bcc23a0799f6aefa/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4e504d2d2532333030303030302e7376673f7374796c653d706c6173746963266c6f676f3d6e706d266c6f676f436f6c6f723d7768697465" alt="npm"/>
               </div>
             </div>
-            <div className="skills-box">
-              <div className="skills-header">
+            <div className="skills-box1">
+              <div className="skills-header-about">
                 <h3>Database</h3>
               </div>
-              <div className="skills-list">
-                <img className='tag' src="https://camo.githubusercontent.com/c59c79370b2ef4d35238a2cc377705b4188a48d8c1a2ff62387d431974f3e483/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6d7973716c2d2532333030662e7376673f7374796c653d706c6173746963266c6f676f3d6d7973716c266c6f676f436f6c6f723d7768697465" alt="MySql"/>
-                <img className='tag' src="https://camo.githubusercontent.com/381b71da18da2cd848e7b184052f8b2488d49d1b5e8e63fe13b0aa11a1e8d3ae/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f706f7374677265732d2532333331363139322e7376673f7374796c653d706c6173746963266c6f676f3d706f737467726573716c266c6f676f436f6c6f723d7768697465" alt="PostgreSql" />
+              <div className="skills-list-about">
+                <img className='tag' style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/c59c79370b2ef4d35238a2cc377705b4188a48d8c1a2ff62387d431974f3e483/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6d7973716c2d2532333030662e7376673f7374796c653d706c6173746963266c6f676f3d6d7973716c266c6f676f436f6c6f723d7768697465" alt="MySql"/>
+                <img className='tag' style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/381b71da18da2cd848e7b184052f8b2488d49d1b5e8e63fe13b0aa11a1e8d3ae/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f706f7374677265732d2532333331363139322e7376673f7374796c653d706c6173746963266c6f676f3d706f737467726573716c266c6f676f436f6c6f723d7768697465" alt="PostgreSql" />
                 <img className='tag' src="https://camo.githubusercontent.com/5cf0cb058a822ec77ef3bd38643acef933c52c8381eb4bf190d9d36481e9cd92/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4d6f6e676f44422d2532333465613934622e7376673f7374796c653d706c6173746963266c6f676f3d6d6f6e676f6462266c6f676f436f6c6f723d7768697465" alt="mongoDB"/>
               </div>
             </div>
-            <div className="skills-box">
-              <div className="skills-header">
+            <div className="skills-box1">
+              <div className="skills-header-about">
                 <h3>Language</h3>
               </div>
-              <div className="skills-list">
-                <img className="tag" src="https://camo.githubusercontent.com/995f28c733b2e7f131c5c7d9f80f7f57123f03269ddb73384a624ed0e23ad45c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632d2532333030353939432e7376673f7374796c653d706c6173746963266c6f676f3d63266c6f676f436f6c6f723d7768697465" alt="C"/>
-                <img className="tag" src="https://camo.githubusercontent.com/2c08fc3d41772227cb08cd66274051a917769fa4330cfd5ca37f7dd8ac1d4826/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632b2b2d2532333030353939432e7376673f7374796c653d706c6173746963266c6f676f3d63253242253242266c6f676f436f6c6f723d7768697465" alt="c++"/>
-                <img className="tag" src="https://camo.githubusercontent.com/6d3b128cd950dff725d5caed37ed86c4039eeec0a861332bd2bc7a73fac74fd1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d706c6173746963266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145" alt="Javascript"/>
-                <img className="tag" src="https://camo.githubusercontent.com/a6a594ae99bb801830151596208ddc1cb603d24e880f10bd694df18b494de8fb/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f707974686f6e2d3336373041303f7374796c653d706c6173746963266c6f676f3d707974686f6e266c6f676f436f6c6f723d666664643534" alt="Python"/>
+              <div className="skills-list-about">
+                <img className="tag" style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/995f28c733b2e7f131c5c7d9f80f7f57123f03269ddb73384a624ed0e23ad45c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632d2532333030353939432e7376673f7374796c653d706c6173746963266c6f676f3d63266c6f676f436f6c6f723d7768697465" alt="C"/>
+                <img className="tag" style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/2c08fc3d41772227cb08cd66274051a917769fa4330cfd5ca37f7dd8ac1d4826/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632b2b2d2532333030353939432e7376673f7374796c653d706c6173746963266c6f676f3d63253242253242266c6f676f436f6c6f723d7768697465" alt="c++"/>
+                <img className="tag" style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/6d3b128cd950dff725d5caed37ed86c4039eeec0a861332bd2bc7a73fac74fd1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6a6176617363726970742d2532333332333333302e7376673f7374796c653d706c6173746963266c6f676f3d6a617661736372697074266c6f676f436f6c6f723d253233463744463145" alt="Javascript"/>
+                <img className="tag" style={{marginRight: "10px"}} src="https://camo.githubusercontent.com/a6a594ae99bb801830151596208ddc1cb603d24e880f10bd694df18b494de8fb/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f707974686f6e2d3336373041303f7374796c653d706c6173746963266c6f676f3d707974686f6e266c6f676f436f6c6f723d666664643534" alt="Python"/>
               </div>
             </div>
-            <div className="skills-box">
-              <div className="skills-header">
+            <div className="skills-box1">
+              <div className="skills-header-about">
                 <h3>IDE Editor</h3>
               </div>
-              <div className="skills-list">
-                <span>VS Code</span>
-                <span>Sublime Text3</span>
-                <span>Pycharm</span>
-                <span>Planet9App Editor</span>
+              <div className="skills-list-about">
+                <span style={{marginRight: "10px"}}>VS Code</span>
+                <span style={{marginRight: "10px"}}>Sublime Text3</span>
+                <span style={{marginRight: "10px"}}>Pycharm</span>
+                <span style={{marginRight: "10px"}}>Planet9App Editor</span>
               </div>
             </div>
           </div>
         </div>
+        <Github/>
       </section>
-      <Github/>
     </main>
   )
 }
