a6544a60ebf9678f17747418e4c4c7571948f886
Abhik Nayak <abhiknayakabhik@gmail.com>
2023-09-13 10:15:47 +0530

    Abhik//13-09-2023

--- portfolio/src/components/Projectdetails.jsx
+++ portfolio/src/components/Projectdetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Typed from 'typed.js';
 import flipKart from "../Assets/flipKart.png";
-
+import Slideshow from './Slideshow';
 
 const Projectdetails = () => {
   // useEffect(() => {
@@ -21,11 +21,13 @@ const Projectdetails = () => {
   return (
     <>
       <main className="wrapper" >
+        
         {/* <!-- -------------- FEATURED BOX ---------------- --> */}
         <section className="featured-box" id="home">
           <div className="featured-image featured-image-prjt">
-            <div className="image" >
-              <img src={flipKart} alt="avatar" />
+            <div className="image-prjt image" >
+            <Slideshow/>
+              {/* <img src={flipKart} alt="avatar" /> */}
             </div>
           </div>
           <div className="featured-text">
@@ -34,18 +36,18 @@ const Projectdetails = () => {
                         <p><span className="typedText"></span></p>
                     </div>
                     <div className="featured-text-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque similique vitae architecto mollitia ex necessitatibus qui impedit molestias eligendi fugiat fuga sunt dignissimos sapiente, reiciendis laudantium, harum hic saepe?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sint ducimus repellat? Distinctio voluptatum modi eligendi incidunt suscipit facilis dicta eaque totam, animi praesentium alias culpa placeat minima. Doloribus, quos!
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic in adipisci nesciunt velit repellat delectus iste odio et ab vitae aspernatur assumenda, expedita, eligendi id officiis incidunt exercitationem. Modi, animi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque similique vitae architecto mollitia ex necessitatibus qui impedit molestias eligendi fugiat fuga sunt dignissimos sapiente, reiciendis laudantium, harum hic saepe?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sint ducimus repellat? Distinctio voluptatum modi eligendi incidunt suscipit facilis dicta eaque totam, animi praesentium alias culpa placeat minima. Doloribus, quos!
                         </p>
                     </div>
                     <div className="featured-text-btn">
                         <button className="btn blue-btn">Hire Me</button>
                         <button className="btn">Download CV <i className="uil uil-file-alt"></i></button>
                     </div>
-                    <div className="social_icons">
-                        <div className="icon"><i className="uil uil-linkedin-alt"></i></div>
+                    {/* <div className="social_icons"> */}
+                        {/* <div className="icon"><i className="uil uil-linkedin-alt"></i></div>
                         <div className="icon"><i className="uil uil-skype"></i></div>
-                        <div className="icon"><i className="uil uil-github-alt"></i></div>
-                    </div>
+                        <div className="icon"><i className="uil uil-github-alt"></i></div> */}
+                    {/* </div> */}
           </div>
 
         </section>
