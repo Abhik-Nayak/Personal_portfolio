8b8d39e72934c1c4e555637f2c9a573728d549a0
Abhik Nayak <abhiknayakabhik@gmail.com>
2025-03-07 19:05:52 +0530

    about section done

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,11 @@
+import AboutSection from "./components/homepage/about";
 import HeroSection from "./components/homepage/hero-section";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <AboutSection/>
     </div>
   );
 }
