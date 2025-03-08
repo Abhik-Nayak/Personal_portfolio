b8101d5c62371c60efe13d1dd09a7ef263d30851
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-03-08 14:45:59 +0530

    code push

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,13 @@
 import AboutSection from "./components/homepage/about";
 import HeroSection from "./components/homepage/hero-section";
+import Skills from "./components/homepage/skills";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <AboutSection/>
+      <Skills/>
     </div>
   );
 }
