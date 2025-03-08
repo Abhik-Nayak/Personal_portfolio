5c4b6f494b7305dbd16482ae56a8540d4ccd9b15
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-03-08 18:00:48 +0530

    code push

--- client/app/page.js
+++ client/app/page.js
@@ -1,4 +1,5 @@
 import AboutSection from "./components/homepage/about";
+import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <AboutSection/>
+      <Experience/>
       <Skills/>
       <Projects/>
     </div>
