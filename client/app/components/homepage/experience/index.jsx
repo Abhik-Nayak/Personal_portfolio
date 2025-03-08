c927fac30c45d27a5396ee04f22d4ba9140c889f
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-03-08 18:08:30 +0530

    code push

--- client/app/components/homepage/experience/index.jsx
+++ client/app/components/homepage/experience/index.jsx
@@ -29,14 +29,9 @@ function Experience() {
       </div>
 
       <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
-          <div className="flex justify-center items-start">
-            <div className="w-full h-full">
-              {/* <AnimationLottie animationPath={experience} /> */}
-            </div>
-          </div>
-
-          <div>
+      
+          
+          
             <div className="flex flex-col gap-6">
               {
                 experiences.map(experience => (
@@ -72,8 +67,8 @@ function Experience() {
                 ))
               }
             </div>
-          </div>
-        </div>
+         
+  
       </div>
     </div>
   );
